46b215b840a8160b1021af9869005e6660ba98eb
GgG <geryuga13@gmail.com>
2026-02-09 15:44:56 +0800

    third commit

--- supabase_config.js
+++ supabase_config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = 'https://idnvhjcxfynnwepmadmq.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkbnZoamN4ZnlubndlcG1hZG1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NjU0NTIsImV4cCI6MjA4NjE0MTQ1Mn0.x7eqFewOiB7VsmS2TbUEwdmy4aTiIIZbVNEOU2u3rZU';
 
 // Inisialisasi Client (Pastikan script supabase-js sudah dimuat di HTML)
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
